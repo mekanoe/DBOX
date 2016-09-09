d7b0386948d77957d3558612e38f04d870dc3317
kayteh <katie@player.me>
2016-09-09 16:55:52 -0500

    server/stats: finish polling

--- src/server/stats/eventApi.js
+++ src/server/stats/eventApi.js
@@ -1,6 +1,6 @@
 const superagent = require('superagent-promise')(require('superagent'), Promise)
-const log = new (require('../logger'))('server/stats/eventApi', true)
-const { Set } = require('immutable')
+const log = new (require('../logger'))('server/stats/eventApi')
+const { OrderedSet } = require('immutable')
 
 const ENDPOINT = 'https://census.daybreakgames.com/s:PomfDBOX/get/ps2:v2'
 
@@ -35,12 +35,31 @@ class EventAPI {
 	// Arguments
 	//   since int
 	//
-	// Returns Promise<Immutable#Set>
-	singlePollVKills(since) {
+	// Returns Promise<Immutable#OrderedSet>
+	pollVKills(since) {
+		let self = this
 		return new Promise((resolve, reject) => {
 			this._request('event', { type: 'VEHICLE_DESTROY', after: since, 'c:limit': 1000 })
 				.then(({body}) => {
-					resolve(Set(body.event_list))
+					resolve(OrderedSet(body.event_list))
+				})
+		})
+	}
+
+
+	////
+	// Primitive polling method for getting deaths, only used for testing.
+	//
+	// Arguments
+	//   since int
+	//
+	// Returns Promise<Immutable#OrderedSet>
+	pollDeaths(since) {
+		let self = this
+		return new Promise((resolve, reject) => {
+			self._request('event', { type: 'DEATH', after: since, 'c:limit': 1000 })
+				.then(({body}) => {
+					resolve(OrderedSet(body.event_list))
 				})
 		})
 	}
@@ -53,6 +72,92 @@ class EventAPI {
 	// Returns int
 	currentTime() {
 		return Math.floor(Date.now() / 1000)
+	}
+
+	////
+	// Starts polling using a poller method every time*seconds, and passes data into to callback.
+	// If callback returns false, the polling will stop.
+	//
+	// Arguments
+	//   fn       func(since int)
+	//   time     int
+	//   callback func(data Immutable#OrderedSet, empty? bool)bool 
+	// Returns PollerController
+	polling(fn, time, callback) {
+
+		log.debug('polling started')
+
+		let ctrl = new PollerController()
+		let lastPoll = this.currentTime()
+		let self = this
+
+		const poll = function() {
+			if (ctrl.isPaused()) {
+				log.debug('polling paused, yielding without data')
+				if (!ctrl.shouldStop) {
+					setTimeout(poll, time*1000)
+				}
+			}
+
+			log.debug(`polling for ${lastPoll}`)
+
+			fn.apply(self,[lastPoll]).then((data) => {
+				let empty = data.size === 0
+
+				let rv = callback(data, empty)
+				if (rv === false) {
+					log.debug('callback returned false, stopping...')
+					ctrl.stop()
+				}
+			}).catch((data) => {
+				log.error('promise failed', data)
+			})
+
+			if (!ctrl.shouldStop) {
+				lastPoll = self.currentTime()
+				setTimeout(poll, time*1000)
+			} else {
+				log.debug('polling was advised to stop')
+			}
+		}
+
+		setTimeout(poll, time*1000)
+
+		return ctrl
+	}
+
+}
+
+////
+// PollerController is a bad aliteration.
+// It gives a simple interface for stopping or pausing a poller.
+//
+// Some important states:
+//  - stopped: stopped will not pass go, and the next time the poller finishes it's run, 
+//             it will not attempt to continue. this cannot be undone.
+//
+//  - paused: this soft-stops data flow, the poller is still waiting, 
+//            but the data acquisition pauses in place.
+class PollerController {
+	constructor() {
+		this.shouldStop = false
+		this.shouldPause = false
+	}
+
+	stop() {
+		this.shouldStop = true
+	}
+
+	pause() {
+		this.shouldPause = true
+	}
+
+	unpause() {
+		this.shouldPause = false
+	}
+
+	isPaused() {
+		return this.shouldPause
 	}
 }
 
--- src/server/stats/eventApi_test.js
+++ src/server/stats/eventApi_test.js
@@ -1,8 +1,53 @@
 const eventApi = require('./eventApi')
 const log = new (require('../logger'))('server/stats/eventApi_test', true)
 
-let api = new eventApi({worlds: ['1']})
+let worlds = ['1']
+let vehicles = ['12']
 
-api.singlePollVKills(api.currentTime() - 300).then((d) => {
-	log.info('got data', d.toJS())
+const api = new eventApi({worlds})
+
+function filter(data) {
+	return data.filter(v => vehicles.indexOf(v.attacker_vehicle_id) !== -1 && worlds.indexOf(v.world_id) !== -1)
+}
+
+function secondStepPoller() {
+	var counter = 1
+	api.polling(api.pollVKills, 1, (data, empty) => {
+		data = filter(data)
+
+		if (data.size === 0) {
+			// log.info('empty data')
+		} else {
+			log.info('got data', data.toJS())
+		}
+
+
+		if ( counter % 10 === 0 ) {
+			log.debug('counter is divisible by 10, stopping...')
+			return false
+		} else {
+			counter = counter + 1
+			// log.debug('counter', counter)
+		}
+	})
+}
+
+const poller = api.polling(api.pollVKills, 1, (data, empty) => {
+	data = filter(data)
+
+	if (data.size === 0) {
+		// log.info('empty data')
+		return
+	}
+
+	log.info('got data', data.toJS())
 })
+
+setTimeout(() => {
+	poller.stop()
+	log.notice('stopped polling via controller')
+
+	log.info('starting second poller')
+	secondStepPoller()
+}, 10000)
+
