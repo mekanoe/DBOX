96b3bf7f283601d62cea23901b5f0740c271ee0e
kayteh <katie@player.me>
2016-09-12 18:51:09 -0500

    server: _route will offer a safe stack page if things go badly on require()

--- server/Dbox.js
+++ server/Dbox.js
@@ -1,3 +1,4 @@
+const log = new (require('./logger'))('server/Dbox')
 
 
 ////
@@ -52,7 +53,6 @@ class Dbox {
 	// @internal
 	// Mount the routes! Makes heavy use of Dbox#_route.
 	_mountRoutes() {
-
 		this._route('./overlay', '/o')
 
 		this._route('./api/streams')
@@ -60,6 +60,8 @@ class Dbox {
 		// Only mount test routes in development.
 		if (process.env.NODE_ENV === 'development') this._route('./api/test')
 
+		// Default to panel if nothing else captured so we can serve a 404 as well.
+		this._route('./panel', '/')
 	}
 
 
@@ -68,23 +70,34 @@ class Dbox {
 	// This is a sugar method so we don't have to keep vars in our route mounter.
 	// If you don't pass a prefix, it'll try to figure it out via the module path.
 	// This lets you put your routes canonical to their URL, e.g. /api/users can sit at ./api/users.js
+	// If something goes wrong during import, e.g. bad path or syntax, a fallback will prevent 
+	// one bad route smashing the entire server.
 	//
 	// Arguments
 	//   modulePath str
 	//   prefix     str?{<null>}
 	_route(modulePath, prefix = null) {
-		let module = require(modulePath)
-
-		if (prefix === null) {
-			// So we weren't given a prefix, so we did some canonical magic. Let's fix the path.
-			// It'll probably start with ., so let's check that and use the rest if so.
-			if (modulePath[0] === '.') {
-				prefix = modulePath.substr(1)
-			} else {
-				// But wait, that wasn't true? Don't be silly.
-				throw `module ${modulePath} couldn't be converted to route path and none was given`
+		let module = null
+		try {
+			module = require(modulePath)
+			if (prefix === null) {
+				// So we weren't given a prefix, so we did some canonical magic. Let's fix the path.
+				// It'll probably start with ., so let's check that and use the rest if so.
+				if (modulePath[0] === '.') {
+					prefix = modulePath.substr(1)
+				} else {
+					// But wait, that wasn't true? Don't be silly.
+					throw `module ${modulePath} couldn't be converted to route path and none was given`
+				}
 			}
+		} catch(e) {
+			log.warn(`module ${modulePath} failed to load. replacing ${prefix} with error page.`, e)
+			module = require('koa-router')().use('*', function *() {
+				this.body = `<!doctype html><style>body{font-family:sans-serif;}</style><p><b>This module (<code>${modulePath}</code>) failed to load.</b></p><pre style="padding:15px;background-color:#eee;border:1px solid #faa;">${e.stack}</pre>`
+				this.status = 500
+			})
 		}
+
 
 		this.router.use(prefix, module.routes(), module.allowedMethods())
 
